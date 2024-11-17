6ce553f0a60564e21e823d1452452899e09a4f1d
unknown <egor@westsib.ru>
2024-11-17 16:33:07 +0700

    Add second

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const imageWrapper = document.querySelector('.images_wrapper')
+
+const bigHoverBlock = document.querySelector('.big_hover_block')
+const lakeHoverBlock = document.querySelector('.lake_hover_block')
+const rightHoverBlock = document.querySelector('.right_hover_block')
+const topHoverBlock = document.querySelector('.top_hover_block')
+const botttomHoverBlock = document.querySelector('.bottom_hover_block')
+const ultraBottomHoverBlock = document.querySelector(
+  '.ultra_bottom_hover_block'
+)
+
+const tramvayLayer = document.querySelector('.tramvay')
+const topBubbles = document.querySelector('.top_bubbles')
+const bottomBubbles = document.querySelector('.bottom_bubbles')
+const krest = document.querySelector('.krest')
+
 const bgImage = document.querySelector('.bg_layer')
-const tram = document.querySelector('.tramvay')
-const leftBubbles = document.querySelector('.left_bubbles')
-const rightBubbles = document.querySelector('.right_bubbles')
-const hoverWrapper = document.querySelector('.hover_wrapper')
 const spinner = document.querySelector('.spinner')
 
 imageWrapper.style.display = 'none'
@@ -30,14 +41,39 @@ imagesInWrapper.forEach((el) => {
   }
 })
 
-hoverWrapper.onmouseover = () => {
-  leftBubbles.classList.add('hovered')
-  rightBubbles.classList.add('hovered')
-  tram.classList.add('tram_hovered')
+bigHoverBlock.onmouseover = (e) => {
+  tramvayLayer.classList.add('lake_hovered')
 }
 
-hoverWrapper.onmouseleave = () => {
-  leftBubbles.classList.remove('hovered')
-  rightBubbles.classList.remove('hovered')
-  tram.classList.remove('tram_hovered')
+tramvayLayer.onmouseover = (e) => {
+  e.target.classList.value.includes('lake_hovered')
+    ? e.target.classList.remove('lake_hovered')
+    : e.target.classList.add('lake_hovered')
+  if (krest.classList.value.includes('krest_hovered')) {
+    krest.classList.remove('krest_hovered')
+  }
+  if (topBubbles.classList.value.includes('top_bubbles_hovered')) {
+    topBubbles.classList.remove('top_bubbles_hovered')
+  }
+  if (bottomBubbles.classList.value.includes('bottom_bubbles_hovered')) {
+    bottomBubbles.classList.remove('bottom_bubbles_hovered')
+  }
+}
+
+lakeHoverBlock.onmouseover = () => {
+  krest.classList.add('krest_hovered')
+}
+
+topHoverBlock.onmouseover = () => {
+  topBubbles.classList.add('top_bubbles_hovered')
+}
+
+rightHoverBlock.onmouseover = () => {
+  bottomBubbles.classList.add('bottom_bubbles_hovered')
+}
+botttomHoverBlock.onmouseover = () => {
+  bottomBubbles.classList.add('bottom_bubbles_hovered')
+}
+ultraBottomHoverBlock.onmouseover = () => {
+  bottomBubbles.classList.add('bottom_bubbles_hovered')
 }
